f96a59d9a6bbd2e11534b16fb39d21387a91f1f1
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-02-05 16:08:52 -0500

    half way throguh 5.6

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,48 @@
 # define the method fake_name(first_name, last_name) then puts the last_name before the first_name then we want to iterate through each letter swapping the vowels to the next vowel in the string aeiou and everything other than the vowels to the next consonant in ther alphabet
 
-def fake_name(first_name, last_name)
-	reverse = "last_name + first_name"
-	puts reverse.chars 
-	reverse.map! do |letter|
-		puts letter
-		letter.next
-		if 
-			letter == [a, e, i, o, u]
+# def fake_name(first_name, last_name)
+# 	reverse = "last_name + first_name"
+# 	puts reverse.chars 
+# 	reverse.map! do |letter|
+# 		puts letter
+# 		if 
+# 			letter.contain? ("bcdfghjklmnpqrstvwxyz")
+# 			puts letter.next
+# 			elsif 
+# 				letter
 			
-			
-		end
+# 		end
+# 	end
+
+def next_vowel(x)
+	if x == "a"
+		puts "e"
+	elsif 
+		 x == "e"
+		 puts  "i"
+	elsif 
+		 x == "i"
+		 puts "o"
+	elsif 
+		 x == "o"
+		 puts "u"
+	else
+		x == "u"
+		puts "a"
+end
+end
+
+vowels = ["a", "e", "i", "o", "u"]
+alphabet = "bcdfghjklmnpqrstvwxyz"
+def letters(x)
+	if vowels.include?(x)
+		next_vowel(x)
+		elsif
+		x == " "
+		puts " "
+	else
+		puts x.next
 	end
+end
+
+puts letters("a")
--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,26 +1,43 @@
 #method takes an array of integers and an integer to search for. Method should return index of item. or nil if not present.
 
 
-arr = [3, 24, 33, 37, 99, 17, 42, 26]
+# arr = [3, 24, 33, 37, 99, 17, 42, 26]
 
 
 
-def search_array(input_array, x)
-#input_array.each do |integer|
-	while input_array.length > 0
-	if input_array.last == x
-		p ((input_array.length) -1)
-	else
+# def search_array(input_array, x)
+# #input_array.each do |integer|
+# 	while input_array.length > 0
+# 	if input_array.last == x
+# 		p ((input_array.length) -1)
+# 	else
 
-	end
-	input_array.delete(input_array.last)
+# 	end
+# 	input_array.delete(input_array.last)
 
-	end	
-end	
+# 	end	
+# end	
 
 
-p search_array(arr, 3)
+# p search_array(arr, 3)
 
 # search for the integer in variable x, upon locating the integer, chop the remaining indeces after the integer and call a .length on the array. returning the .length - 1 for the index. 
 # if input_array.last == x
 #p ((input_array.length) -1)
+
+# Inputs x number of indeces for the array, also follows the formula of fibonacci which is the previous index + the current = the new
+
+
+
+	def fib(x)
+ a = 0
+ b = 1
+  fib_array = []
+  x.times do
+    fib_array << a 
+    a, b = b, a+b
+  end 
+  fib_array
+end
+
+p fib(6)
