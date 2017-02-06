608b2de0d2e53f6aab8646dd0a13f526e0df419e
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-02-06 17:15:05 -0500

    got as far as I could with solo challange

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,48 +1,80 @@
 # define the method fake_name(first_name, last_name) then puts the last_name before the first_name then we want to iterate through each letter swapping the vowels to the next vowel in the string aeiou and everything other than the vowels to the next consonant in ther alphabet
+#I can get the methods to work indivudually but I'm struggling to get them to work as a whole... I know I can't get help on this as a solo project but I will have to ask questions in office hours so I can better understand it after it is handed in
 
-# def fake_name(first_name, last_name)
-# 	reverse = "last_name + first_name"
-# 	puts reverse.chars 
-# 	reverse.map! do |letter|
-# 		puts letter
-# 		if 
-# 			letter.contain? ("bcdfghjklmnpqrstvwxyz")
-# 			puts letter.next
-# 			elsif 
-# 				letter
-			
-# 		end
-# 	end
+until quit == "quit"
+puts "to quit the name changer type quit"
+quit = gets.chomp
+puts "first name:"
+first_name = gets.chomp
+puts "last name:"
+last_name = gets.chomp
 
-def next_vowel(x)
-	if x == "a"
-		puts "e"
-	elsif 
-		 x == "e"
-		 puts  "i"
-	elsif 
-		 x == "i"
-		 puts "o"
-	elsif 
-		 x == "o"
-		 puts "u"
-	else
-		x == "u"
-		puts "a"
-end
-end
 
-vowels = ["a", "e", "i", "o", "u"]
-alphabet = "bcdfghjklmnpqrstvwxyz"
-def letters(x)
-	if vowels.include?(x)
-		next_vowel(x)
-		elsif
-		x == " "
-		puts " "
-	else
-		puts x.next
+
+
+def fake_name(first_name, last_name)
+	reverse = "last_name + first_name"
+	puts reverse.chars 
+	reverse.map! do |letter|
+		puts letter
+		if 
+			letter.contain? ("bcdfghjklmnpqrstvwxyz")
+			puts letter.next
+			elsif 
+				def next_vowel(letter)
+							if x == "a"
+								puts "e"
+							elsif 
+		 						x == "e"
+		 						puts  "i"
+							elsif 
+		 						x == "i"
+		 						puts "o"
+							elsif 
+								x == "o"
+		 						puts "u"
+							else
+								x == "u"
+								puts "a"
+							end
+				end
+
+				def letters(letter)
+				vowels = ["a", "e", "i", "o", "u"]
+						if vowels.include?(letter)
+							 next_vowel(letter)
+						elsif
+								letter == " "
+								puts " "
+						elsif 
+								letter == "-"
+								puts "-"
+						else
+								puts letter.next
+					end
+			end
+
+false_name = letters(letter) 
+puts false_name
+puts "#{false_name} is actually #{first_name + last_name}"		
+		end
 	end
-end
+puts fake_name(first_name, last_name)
 
-puts letters("a")
+
+
+
+# alphabet = "bcdfghjklmnpqrstvwxyz"
+# def letters(x)
+# 	vowels = ["a", "e", "i", "o", "u"]
+# 	if vowels.include?(x)
+# 		next_vowel(x)
+# 		elsif
+# 		x == " "
+# 		puts " "
+# 	else
+# 		puts x.next
+# 	end
+# end
+
+# end
