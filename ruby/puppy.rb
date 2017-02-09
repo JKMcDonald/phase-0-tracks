9aa85990b61c98bcca738d3d2ccf2686b11fcd5f
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-02-08 22:18:46 -0500

    easy 6.1 Puppy class instance

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+	class ----------
+		puppy
+
+	Characteristics ----------
+	Nose type: varies
+	Friendly: yes
+	Tail: yes
+	Name: varies
+	Color: varies
+
+	Behavior ----------
+	Jump
+	Hug
+	Wag Tail
+	Sleep
+	Drool
+	Bark
+	
+end
