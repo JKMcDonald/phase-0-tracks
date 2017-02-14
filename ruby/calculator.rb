41ba60ce021d9b0fe9d6105736b40d5d61c8228b
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-02-14 16:20:14 -0500

    divide methods from loops, seperate. use .store on an empty hash iterate with a puts only on the string

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,16 +1,10 @@
 continue = ""
-until continue == "done"
-puts "type anything to continue or done to exit (if select done complete one more calculation to exit)"
-    continue = gets.chomp
-puts "What would you like your first variable's value to be?"
-    x = gets.chomp.to_i
-puts "What would you like your operation to be (+,-,*,/)"
-    operator = gets.chomp
-puts "What would you like your final variable's value to be?"
-    z = gets.chomp.to_i
+solutions = {}
+
 def calculate(x, operator, z)
+	
 	if operator == "+"
-		return x + z
+		return x + z 
 	elsif operator == "-"
 		return x - z
 	elsif operator == "*"
@@ -20,9 +14,28 @@ def calculate(x, operator, z)
 	else
 		puts "error: Please eneter a valid integer and/or operation after the questions: Please try again, thank you."
 	end
+ end
+
+until continue == "done"
+puts "type anything to continue or done to exit (if select done complete one more calculation to exit)"
+    continue = gets.chomp
+puts "What would you like your equation to be?"
+ equation = gets.chomp
+
+ string_array = equation.split(" ")
+
+ x = string_array[0].to_i
+ operator = string_array[1]
+ z = string_array[2].to_i
+
+ p calculate(x, operator, z)
+
+ solutions.store(equation, calculate(x, operator, z))
+
 end
-puts "your solution is:"
-p calculate(x, operator, z)
+ 	puts "your solution is:"
+	solutions.each do |equals, solution|
+		puts "#{equals} = #{solution}"
 # metho = calculate(x, operator, z)
 # solution = []
 # metho.map { |answer| solution << answer }
